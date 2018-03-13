e1833f33a9a3bbf16d649cc9237d2e8648739324
Jose Barrios <josedbarrios7@gmail.com>
2018-03-13 15:33:44 -0400

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,94 +12,101 @@ use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth:api')->except('index','show');
-    }
+		public function __construct()
+		{
+				$this->middleware('auth:api')->except('index','show');
+		}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-       return ProductCollection::collection(Product::paginate(20));
-    }
+		/**
+		 * Display a listing of the resource.
+		 *
+		 * @return \Illuminate\Http\Response
+		 */
+		public function index()
+		{
+			 return ProductCollection::collection(Product::paginate(20));
+		}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+		/**
+		 * Show the form for creating a new resource.
+		 *
+		 * @return \Illuminate\Http\Response
+		 */
+		public function create()
+		{
+				//
+		}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(ProductRequest $request)
-    {
-        $product = new Product;
-        $product->name= $request->name;
-        $product->detail = $request->description;
-        $product->stock = $request->stock;
-        $product->price = $request->price;
-        $product->discount = $request->discount;
-        $product->save();
+		/**
+		 * Store a newly created resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request  $request
+		 * @return \Illuminate\Http\Response
+		 */
+		public function store(ProductRequest $request)
+		{
+				$product = new Product;
+				$product->name= $request->name;
+				$product->detail = $request->description;
+				$product->stock = $request->stock;
+				$product->price = $request->price;
+				$product->discount = $request->discount;
+				$product->save();
 
-        return response([
-           'data' => new ProductResource($product)      
-        ], Response::HTTP_CREATED);
-    }
+				return response([
+					 'data' => new ProductResource($product)      
+				], Response::HTTP_CREATED);
+		}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        return new ProductResource($product);
-    }
+		/**
+		 * Display the specified resource.
+		 *
+		 * @param  \App\Model\Product  $product
+		 * @return \Illuminate\Http\Response
+		 */
+		public function show(Product $product)
+		{
+				return new ProductResource($product);
+		}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
+		/**
+		 * Show the form for editing the specified resource.
+		 *
+		 * @param  \App\Model\Product  $product
+		 * @return \Illuminate\Http\Response
+		 */
+		public function edit(Product $product)
+		{
+				//
+		}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
+		/**
+		 * Update the specified resource in storage.
+		 *
+		 * @param  \Illuminate\Http\Request  $request
+		 * @param  \App\Model\Product  $product
+		 * @return \Illuminate\Http\Response
+		 */
+		public function update(Request $request, Product $product)
+		{
+				$request['detail'] = $request->description;
+				unset($request['description']);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+				$product->update($request->all());
+
+				return response([
+					 'data' => new ProductResource($product)      
+				], Response::HTTP_CREATED);
+		}
+
+		/**
+		 * Remove the specified resource from storage.
+		 *
+		 * @param  \App\Model\Product  $product
+		 * @return \Illuminate\Http\Response
+		 */
+		public function destroy(Product $product)
+		{
+				//
+		}
 }
