80724b2c56a47a0ac17261f747ffd7f77c632be1
Jose Barrios <josedbarrios7@gmail.com>
2018-03-13 13:59:00 -0400

    Passport Installed

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,29 +6,29 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-            'name' => $this->name,
-            'description' => $this->detail,
-            'price' => $this->price,
-            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
-            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
-            'discount' => $this->discount,
-            'rating' => $this->reviews->count() > 0 ?
-                round($this->reviews->sum('star')/$this->reviews->count(),2)
-                :
-                'No rating yet',
-            'href' => [
-                'reviews' => route('reviews.index', $this->id)
-            ]
+		/**
+		 * Transform the resource into an array.
+		 *
+		 * @param  \Illuminate\Http\Request  $request
+		 * @return array
+		 */
+		public function toArray($request)
+		{
+				return [
+						'name' => $this->name,
+						'description' => $this->detail,
+						'price' => $this->price,
+						'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+						'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+						'discount' => $this->discount,
+						'rating' => $this->reviews->count() > 0 ?
+								round($this->reviews->sum('star')/$this->reviews->count(),2)
+								:
+								'No rating yet',
+						'href' => [
+								'reviews' => route('reviews.index', $this->id)
+						]
 
-        ];
-    }
+				];
+		}
 }
